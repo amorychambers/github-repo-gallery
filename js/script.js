75ba4f205c2b5cb08dc174e1e66c91470133a531
Amory <108595086+amorychambers@users.noreply.github.com>
2022-08-23 14:46:11 +0100

    Added API to display specific repo info

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const profileHeader = document.querySelector(".overview");
+const repoSection = document.querySelector(".repos");
 const repoList = document.querySelector(".repo-list");
+const repoData = document.querySelector(".repo-data");
 const username = "amorychambers";
 
 
@@ -46,3 +48,38 @@ function displayRepos (repos){
   };
 };
 // Displays public repo data
+
+repoList.addEventListener("click", function(e){
+  if (e.target.matches("h3")){
+    let repoName = e.target.innerText;
+    singleRepo(repoName);
+  }
+});
+// Displays more detailed info on the repo that the user clicks
+
+async function singleRepo (repoName){
+  let data = await fetch (`https://api.github.com/repos/${username}/${repoName}`);
+  let singleRepoInfo = await data.json();
+  let fetchLanguages = await fetch(`${singleRepoInfo.languages_url}`);
+  let languageData = await fetchLanguages.json();
+  let languages = [];
+  for (let key in languageData){
+    languages.push(key);
+  };
+  displayRepoInfo(singleRepoInfo, languages);
+};
+// Collects specific data on one repo
+
+function displayRepoInfo (singleRepoInfo, languages){
+  repoData.innerHTML = "";
+  let repoCard = document.createElement("div");
+  repoCard.innerHTML = `<h3>Name: ${singleRepoInfo.name}</h3>
+    <p>Description: ${singleRepoInfo.description}</p>
+    <p>Default Branch: ${singleRepoInfo.default_branch}</p>
+    <p>Languages: ${languages.join(", ")}</p>
+    <a class="visit" href="${singleRepoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>`;
+    repoData.append(repoCard);
+    repoSection.classList.add("hide");
+    repoData.classList.remove("hide");
+};
+// Displays the card with all the specific repo information
