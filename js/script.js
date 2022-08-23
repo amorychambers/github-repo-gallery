bca8427359f7438c608536a1ed505dfc4c3f27ab
Amory <108595086+amorychambers@users.noreply.github.com>
2022-08-23 13:31:26 +0100

    Added APIs to fetch profile and repo data

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 const profileHeader = document.querySelector(".overview");
+const repoList = document.querySelector(".repo-list");
 const username = "amorychambers";
 
-async function fetchData (){
+
+getProfile();
+
+async function getProfile (){
     const data = await fetch(`https://api.github.com/users/${username}`);
     const profileInfo = await data.json();
-    displayData(profileInfo);
-    console.log(profileInfo);
+    displayProfile(profileInfo);
 };
+// Fetches profile data
 
-function displayData (profileInfo){
+function displayProfile (profileInfo){
     let userInfo = document.createElement("div");
     userInfo.classList.add("user-info");
     userInfo.innerHTML = `
@@ -22,6 +26,23 @@ function displayData (profileInfo){
           <p><strong>Number of public repos:</strong> ${profileInfo.public_repos}</p>
         </div>`;
         profileHeader.append(userInfo);
+        getRepos();
 };
+// Displays profile data
 
-fetchData();
+async function getRepos (){
+  const data = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+  const repoInfo = await data.json();
+  displayRepos(repoInfo);
+};
+// Fetches public repo data
+
+function displayRepos (repos){
+  for (let repo of repos){
+    let listItem = document.createElement("li");
+    listItem.innerHTML = `<h3>${repo.name}</h3>`;
+    listItem.classList.add("repo");
+    repoList.append(listItem);
+  };
+};
+// Displays public repo data
